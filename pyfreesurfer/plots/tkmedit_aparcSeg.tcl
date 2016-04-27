f91768572a7165b45a756f0f840870a4304d50fb
David Goyard <david.goyard@cea.fr>
2016-04-27 13:36:15 +0200

    pyfreesurfer: add aparc segmentation QA plots

---
+++ pyfreesurfer/plots/tkmedit_aparcSeg.tcl
@@ -0,0 +1,22 @@
+# tkmedit_aparc+aseg.tcl
+
+#-------------------------------------
+# Load surfaces and colormap:
+
+puts "Load segmentation volume and colormap"
+LoadSegmentationVolume nu.mgz aparc+aseg.mgz %(LOOKUPTABLE)s
+
+#-------------------------------------
+# Slice volume:
+
+SetOrientation %(ORIENT)s
+SetCursor 0 0 0 0
+for { set slice %(START)s } { $slice <= %(END)s } { incr slice %(INCR)s } {
+    SetSlice $slice
+    RedrawScreen
+    SaveRGB %(RGBFILE)s
+}
+
+UnloadSurface 0
+UnloadSurface 1
+QuitMedit
